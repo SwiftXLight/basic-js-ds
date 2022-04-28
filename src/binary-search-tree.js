e7dfb092d5ff0fdc9da446f17d0d462c2b3a14ae
SwiftLight <andrevpanchenko@gmail.com>
2022-04-28 20:27:56 +0300

    feat: make binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -70,9 +70,41 @@ class BinarySearchTree {
   }
 
   remove(data) {
-    if (this._node === null) {
-      return;
-    }
+    this._node = removeNode(this._node, data);
+
+		function removeNode(node, data) {
+			if (!node) return null;
+
+			if (data < node.data) {
+				node.left = removeNode(node.left, data);
+				return node;
+			} else if (data > node.data) {
+				node.right = removeNode(node.right, data);
+				return node;
+			} else {
+				if (!node.left && !node.right) return null;
+
+				if (!node.left) {
+					node = node.right;
+					return node;
+				}
+
+				if (!node.right) {
+					node = node.left;
+					return node;
+				}
+			}
+
+			let minFromRight = node.right;
+			while (minFromRight.left) {
+				minFromRight = minFromRight.left;
+			}
+			node.data = minFromRight.data;
+
+			node.right = removeNode(node.right, minFromRight.data);
+
+			return node;
+		}
   }
 
   min() {
